750b30049dfc9ef1515793b49f2791babcf98bae
Azerlag <111231386+Azerlag@users.noreply.github.com>
2022-08-14 20:03:58 +0400

    Delete transfer directory

--- transfer/temp-1.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  $myFile = "../txt/in-1.txt";
-  $fh = fopen($myFile, 'r');
-  $theData1 = fread($fh, filesize($myFile));
-  fclose($fh);
-  echo $theData1;
-?>
--- transfer/temp-2.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  $myFile = "../txt/in-2.txt";
-  $fh = fopen($myFile, 'r');
-  $theData = fread($fh, filesize($myFile));
-  fclose($fh);
-  echo $theData;
-?>
